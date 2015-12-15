83de2d4d0624b107c0f08fb9f0462ba89ea9ffb9
ritan22 <tllont29@gmail.com>
2015-12-15 19:31:40 +0900

    add meiro.html

---
+++ public/meiro/js/meiro.js
@@ -0,0 +1,90 @@
+var ary = [
+  [1, 0, 1, 0, 1], //1が黒、0が白
+  [0, 2, 0, 0, 0], //2はプレーヤ
+  [1, 0, 1, 0, 1],
+];
+
+var position = [1, 1];
+
+// for (var i = 0; i < 2; i++) {
+//   document.write("<p>");
+//
+//   for (var j = 0; j < 2; j++) {
+//     document.write("[" + i + "][" + j + "] = " + ary[i][j] + "<br />");
+//   }
+//
+//   document.write("</p>");
+// }
+
+function init() {
+  var map = document.getElementById('map');
+
+  map.innerHTML = "";
+  for (var i = 0; i < ary.length; i++) {
+
+    for (var j = 0; j < ary[i].length; j++) {
+      block = document.createElement('div');
+
+      if (ary[i][j] == 1) {
+        var black = document.createElement('div');
+        black.className = 'block black';
+        map.appendChild(black);
+      } else if (ary[i][j] == 0) {
+        var white = document.createElement('div');
+        white.className = 'block white';
+        map.appendChild(white);
+      } else if (ary[i][j] == 2) {
+        var character = document.createElement('img');
+        character.className = "block";
+        character.src = 'human.png';
+        map.appendChild(character);
+      } else {
+        alert("blockError");
+      }
+
+    }
+    var rn = document.createElement('div');
+    rn.className = 'clr';
+    map.appendChild(rn);
+  }
+}
+
+function test() {
+  alert();
+}
+
+function move_up() {
+  if ((ary[position[0] - 1][position[1]]) == 0) {
+    ary[position[0]][position[1]] = 0;
+    ary[position[0] - 1][position[1]] = 2;
+    position = [position[0] - 1, position[1]];
+  }
+  init();
+}
+
+function move_down() {
+  if ((ary[position[0] + 1][position[1]]) == 0) {
+    ary[position[0]][position[1]] = 0;
+    ary[position[0] + 1][position[1]] = 2;
+    position = [position[0] + 1, position[1]];
+  }
+  init();
+}
+
+function move_right() {
+  if ((ary[position[0]][position[1] + 1]) == 0) {
+    ary[position[0]][position[1]] = 0;
+    ary[position[0]][position[1] + 1] = 2;
+    position = [position[0], position[1] + 1];
+  }
+  init();
+}
+
+function move_left() {
+  if ((ary[position[0]][position[1] - 1]) == 0) {
+    ary[position[0]][position[1]] = 0;
+    ary[position[0]][position[1] - 1] = 2;
+    position = [position[0], position[1] - 1];
+  }
+  init();
+}
