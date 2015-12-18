4dfa60dc0a2ad14e597b0245354a4d3b5b32ff48
matsuneko22 <matsuneko22@outlook.jp>
2015-12-19 00:19:45 +0900

    create ManageSession class

--- app/controllers/WebSocket.java
+++ app/controllers/WebSocket.java
@@ -7,16 +7,16 @@ import java.util.*;
 
 import models.*;
 
-public class WebSocket extends WebSocketController { 
+public class WebSocket extends WebSocketController {
 	public static void sendms(){
 		while(inbound.isOpen()){
-				
+			
 		}
 	}
-	
+
 	public static void sendpoint(){
 		while(inbound.isOpen()){
-			
+
 		}
 	}
 }
---
+++ app/models/ManageSession.java
@@ -0,0 +1,7 @@
+package models;
+
+import java.util.*;
+
+static class ManageSession{
+  //public static List<Scope.Session> sessions = new ArrayList();
+}
