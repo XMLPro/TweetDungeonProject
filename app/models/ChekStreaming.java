f6449a7e1aef6c1f3ea5e759fd27ee82e0359b26
matsuneko22 <matsuneko22@outlook.jp>
2015-12-18 23:47:47 +0900

    getHashとwebsocketのadapterになるクラスの追加

---
+++ app/models/ChekStreaming.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class CheckSt{
+	private static CheckSt cs = new CheckSt();
+	private	List<String> twis = new ArrayList<String>();
+	private boolean flag;
+
+	// コンストラクタ
+	public CheckSt(){
+		flag = false;
+	}
+
+	//Instanceを返す
+	public static CheckSt getInstance(){
+		return cs;
+	}
+
+	// Tweetを受け取りフラグを立てるクラス
+	public void postSt(String twi){
+		twis.add(twi);
+		cs.UpFlag();
+	}
+
+	// falgをたてます
+	public void upFlag(){
+		flag = true;
+	}
+
+	// flagを返す
+	public boolean getFalg(){
+		return cs.flag;
+	}
+
+	// tweetを貯めたlistを返す
+	public List<String> getTwis(){
+		List<String> tmp = cs.twis;
+		flag = false;
+		cs.twis = new ArrayList<String>();
+		return tmp;
+	}
+}
