be3ef0429cbfa46d4975b71930fea987844c0b74
kazuki matsushima <kazuki matsushima>
2015-12-18 19:58:46 +0900

    同期を取るためのごめんなさいコミット

---
+++ Untitled-1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class CheckSt{
+	
+	List twis = new List
+	public static void postSt(String twi){
+		
+	}
+}
---
+++ app/controllers/WebSocket.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class WebSocket extends WebSocketController { 
+	public static void sendms(){
+		while(inbound.isOpen()){
+				
+		}
+	}
+	
+	public static void sendpoint(){
+		while(inbound.isOpen()){
+			
+		}
+	}
+}
