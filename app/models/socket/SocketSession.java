ca4192a1578e6dc3971bc5ad3142c280fb1f624e
matsuneko22 <matsuneko22@outlook.jp>
2015-12-20 03:57:27 +0900

    file added

---
+++ app/models/socket/SocketSession.java
@@ -0,0 +1,34 @@
+package models.socket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SocketSession {
+	private String socket_id;
+	private List<String> tweets = new ArrayList<String>();
+	
+	public SocketSession(String socket_id){
+		this.socket_id = socket_id;
+	}
+	
+	public void stockTweets(String tweet){
+		tweets.add(tweet);
+	}
+	
+	public List getStockedTweets(){
+		return tweets;
+	}
+	
+	public void removeStockedTweet(String str){
+		tweets.remove(tweets.indexOf(str));
+	}
+	
+	public String getSocketId(){
+		return socket_id;
+	}
+	
+	public void clean(){
+		tweets.clear();
+	}
+
+}
