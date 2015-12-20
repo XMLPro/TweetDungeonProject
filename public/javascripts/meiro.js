1c882a6b75befc941c6849ef660549633a08f9dd
sinshutukibotu <c0114292a2@edu.teu.ac.jp>
2015-12-20 09:50:04 +0900

    0.5秒ごとにwsを要求

--- public/javascripts/meiro.js
+++ public/javascripts/meiro.js
@@ -18,82 +18,110 @@ socket.onmessage = function(event) {
     }
 }
 var ary = [
-  [1, 0, 1, 0, 1], //1が黒、0が白
-  [0, 2, 0, 0, 0], //2はプレーヤ
-  [1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1], //1が黒、0が白
+    [0, 2, 0, 0, 0], //2はプレーヤ
+    [1, 0, 1, 0, 1],
 ];
+
+
+var loop = function () {
+    setTimeout(function () {
+      socket.send("update");
+        loop();
+    }, 500);
+}
+
+//function posting() {
+//    jQuery.ajax(
+//            "/map_st", {
+//                type: "WS",
+//                dataType: 'text',
+//                data: {
+//                    "update": "update",
+//                },
+//                success: function (post) {
+//                    ary = post.map;
+//                    init();
+//                }
+//            }
+//            );
+//
+//    console.log("post");
+//}
+
+loop();
 
 var position = [1, 1];
 
 function init() {
-  var map = document.getElementById('map');
+    var map = document.getElementById('map');
 
-  map.innerHTML = "";
-  for (var i = 0; i < ary.length; i++) {
+    map.innerHTML = "";
+    for (var i = 0; i < ary.length; i++) {
 
-    for (var j = 0; j < ary[i].length; j++) {
-      block = document.createElement('div');
+        for (var j = 0; j < ary[i].length; j++) {
+            block = document.createElement('div');
 
-      if (ary[i][j] == 1) {
-        var black = document.createElement('div');
-        black.className = 'block black';
-        map.appendChild(black);
-      } else if (ary[i][j] == 0) {
-        var white = document.createElement('div');
-        white.className = 'block white';
-        map.appendChild(white);
-      } else if (ary[i][j] == 2) {
-        var character = document.createElement('img');
-        character.className = "block";
-        character.src = '../../../public/images/human.png';
-        map.appendChild(character);
-      } else {
-        alert("blockError");
-      }
+            if (ary[i][j] == 1) {
+                var black = document.createElement('div');
+                black.className = 'block black';
+                map.appendChild(black);
+            } else if (ary[i][j] == 0) {
+                var white = document.createElement('div');
+                white.className = 'block white';
+                map.appendChild(white);
+            } else if (ary[i][j] == 2) {
+                var character = document.createElement('img');
+                character.className = "block";
+                character.src = '../../../public/images/human.png';
+                map.appendChild(character);
+            } else {
+                alert("blockError");
+            }
 
+        }
+        var rn = document.createElement('div');
+        rn.className = 'clr';
+        map.appendChild(rn);
     }
-    var rn = document.createElement('div');
-    rn.className = 'clr';
-    map.appendChild(rn);
-  }
 }
 
 function test() {
-  alert();
+    alert();
 }
 
 function move_up() {
-  if ((ary[position[0] - 1][position[1]]) == 0) {
-    ary[position[0]][position[1]] = 0;
-    ary[position[0] - 1][position[1]] = 2;
-    position = [position[0] - 1, position[1]];
-  }
-  init();
+    if ((ary[position[0] - 1][position[1]]) == 0) {
+        ary[position[0]][position[1]] = 0;
+        ary[position[0] - 1][position[1]] = 2;
+        position = [position[0] - 1, position[1]];
+    }
+    init();
 }
 
 function move_down() {
-  if ((ary[position[0] + 1][position[1]]) == 0) {
-    ary[position[0]][position[1]] = 0;
-    ary[position[0] + 1][position[1]] = 2;
-    position = [position[0] + 1, position[1]];
-  }
-  init();
+    if ((ary[position[0] + 1][position[1]]) == 0) {
+        ary[position[0]][position[1]] = 0;
+        ary[position[0] + 1][position[1]] = 2;
+        position = [position[0] + 1, position[1]];
+    }
+    init();
 }
 
 function move_right() {
-  if ((ary[position[0]][position[1] + 1]) == 0) {
-    ary[position[0]][position[1]] = 0;
-    ary[position[0]][position[1] + 1] = 2;
-    position = [position[0], position[1] + 1];
-  }
-  init();
+    if ((ary[position[0]][position[1] + 1]) == 0) {
+        ary[position[0]][position[1]] = 0;
+        ary[position[0]][position[1] + 1] = 2;
+        position = [position[0], position[1] + 1];
+    }
+    init();
 }
 
 function move_left() {
-  if ((ary[position[0]][position[1] - 1]) == 0) {
-    ary[position[0]][position[1]] = 0;
-    ary[position[0]][position[1] - 1] = 2;
-    position = [position[0], position[1] - 1];
-  }
-  init();
+    if ((ary[position[0]][position[1] - 1]) == 0) {
+        ary[position[0]][position[1]] = 0;
+        ary[position[0]][position[1] - 1] = 2;
+        position = [position[0], position[1] - 1];
+    }
+    init();
 }
