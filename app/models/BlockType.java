9653b9b656cc481cf50edd3770476748a56d0e62
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-12-05 09:23:41 +0900

    delete

--- app/models/BlockType.java
+++
@@ -1,8 +0,0 @@
-package models;
-public enum BlockType {
-	ROAD,
-	WALL,
-	PLAYER,
-	START,
-	GOAL
-}
--- app/models/hashGet/Key.java
+++
@@ -1,9 +0,0 @@
-package models.hashGet;
-
-public class Key {
-	protected static final String CONSUMER_KEY          = "";
-	protected static final String CONSUMER_SECRET       = "";
-	protected static final String ACCESS_TOKEN          = "";
-	protected static final String ACCESS_TOKEN_SECRET   = "";
-}
-
