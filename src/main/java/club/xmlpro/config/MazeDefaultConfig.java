021fd62da299f68a2d5a75be203951bdceb182c0
NaotoSuzuki <c0114292a2@edu.teu.ac.jp>
2016-12-11 07:10:54 +0900

    マップの大きさ調整

--- src/main/java/club/xmlpro/config/MazeDefaultConfig.java
+++ src/main/java/club/xmlpro/config/MazeDefaultConfig.java
@@ -1,8 +1,8 @@
 package club.xmlpro.config;
 
 public class MazeDefaultConfig {
-    public static final int DUNGEON_MAP_WIDTH = 33;
-    public static final int DUNGEON_MAP_HEIGHT = 33;
+    public static final int DUNGEON_MAP_WIDTH = 19;
+    public static final int DUNGEON_MAP_HEIGHT = 19;
     public static final int CHARACTER_START_X = 4;
     public static final int CHARACTER_START_Y = 6;
 }
