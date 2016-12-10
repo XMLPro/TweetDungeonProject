c71ea8c121ffe65cf9f6fcc3d7edea322a9b8d01
wildFoxN <ninetailsosaki@gmail.com>
2016-12-11 04:10:08 +0900

    start spplication

--- src/main/java/club/xmlpro/TweetDungeonProject.java
+++ src/main/java/club/xmlpro/TweetDungeonProject.java
@@ -1,12 +1,7 @@
 package club.xmlpro;
 
-import club.xmlpro.event.AnalyzeEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class TweetDungeonProject {
